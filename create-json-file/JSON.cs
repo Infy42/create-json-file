7ff8f517cbe9a799aa85e10f5dd563de42f02cb7
Infy42 <milovanmilic98@gmail.com>
2023-05-14 22:02:22 +0200

    c#
    create a json file

---
+++ create-json-file/JSON.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_json_file
+{
+    class JSON
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string birth_date { get; set; }
+        public int salary { get; set; }
+        public string position { get; set; }
+        public string hired_date { get; set; }
+    }
+}
---
+++ create-json-file/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace create_json_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingClass();
+
+            Console.Read();
+        }
+
+        static void UsingClass()
+        {
+            DateTime dateTime = DateTime.Now;
+            DateTime dateTimeTwo = dateTime.AddDays(20);
+
+            JSON json = new JSON()
+            {
+                id = 1,
+                name = "Rapheal Colluci",
+                email = "rafa@gmail.com",
+                birth_date = dateTime.ToString(),
+                salary = 105000,
+                position = "C# Software Developer",
+                hired_date = dateTimeTwo.ToString(),
+            };
+
+            string serializedJson = JsonSerializer.Serialize(json);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\createdJson.json";
+
+            File.WriteAllText(path, serializedJson);
+        }
+
+        static void UsingAnonymous()
+        {
+            DateTime dateTime = DateTime.Now;
+            DateTime dateTimeTwo = dateTime.AddDays(20);
+
+            var json = new
+            {
+                id = 1,
+                name = "Rapheal Colluci",
+                email = "rafa@gmail.com",
+                birth_date = dateTime.ToString(),
+                salary = 105000,
+                position = "C# Software Developer",
+                hired_date = dateTimeTwo.ToString(),
+            };
+
+            string serializedJson = JsonSerializer.Serialize(json);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\createdJson.json";
+
+            File.WriteAllText(path, serializedJson);
+        }
+    }
+}
